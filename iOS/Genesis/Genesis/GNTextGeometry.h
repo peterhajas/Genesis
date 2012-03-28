1fdca365b3ca3acf76efc2e3248522d24b7d8861
Peter Hajas <peterhajas@gmail.com>
2012-03-28 16:59:51 -0400

    Added GNTextGeometry (replacing GNTextRange) for text geometry operations.

---
+++ iOS/Genesis/Genesis/GNTextGeometry.h
@@ -0,0 +1,13 @@
+//
+//  GNTextGeometry.h
+//  Genesis
+//
+//  Created by Peter Hajas on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNTextGeometry : NSObject
+
+@end
---
+++ iOS/Genesis/Genesis/GNTextGeometry.m
@@ -0,0 +1,13 @@
+//
+//  GNTextGeometry.m
+//  Genesis
+//
+//  Created by Peter Hajas on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GNTextGeometry.h"
+
+@implementation GNTextGeometry
+
+@end
