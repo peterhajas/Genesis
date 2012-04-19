094b7faadfacd3c347aab6845a12210595466d57
Peter Hajas <peterhajas@gmail.com>
2012-04-19 01:12:26 -0400

    Added a new UITableViewCell subclass, GNTextAutocompleteSuggestionCell, for autocomplete suggestions.

--- iOS/Genesis/Genesis/GNTextAutocompleteInputView.m
+++ iOS/Genesis/Genesis/GNTextAutocompleteInputView.m
@@ -14,6 +14,7 @@
  */
 
 #import "GNTextAutocompleteInputView.h"
+#import "GNTextAutocompleteSuggestionCell.h"
 
 @implementation GNTextAutocompleteInputView
 
@@ -56,9 +57,9 @@
 
 -(UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-    UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                                   reuseIdentifier:@"kGNAutoCompleteTableViewCellIdentifier"];
-    [[cell textLabel] setText:[autocompleteSuggestionsForCurrentWord objectAtIndex:[indexPath row]]];
+    NSString* suggestion = [autocompleteSuggestionsForCurrentWord objectAtIndex:[indexPath row]];
+    GNTextAutocompleteSuggestionCell* cell = [[GNTextAutocompleteSuggestionCell alloc] initWithText:suggestion];
+    
     return cell;
 }
 
---
+++ iOS/Genesis/Genesis/GNTextAutocompleteSuggestionCell.m
@@ -0,0 +1,86 @@
+/* Copyright (c) 2012, individual contributors
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#import "GNTextAutocompleteSuggestionCell.h"
+#import "GNTextGeometry.h"
+
+@implementation GNTextAutocompleteSuggestionCell
+
+-(id)initWithText:(NSString*)text
+{
+    self = [super initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:@"kGNAutoCompleteCell"];
+    if(self)
+    {
+        // Create a UIView for our content
+        UIView* content = [[UIView alloc] initWithFrame:[self frame]];
+        
+        // Set background gradient
+        gradient = [CAGradientLayer layer];
+        [gradient setFrame:[self bounds]];
+        [gradient setColors:[NSArray arrayWithObjects:(id)[[UIColor whiteColor] CGColor],
+                                                   (id)[[UIColor lightGrayColor] CGColor],
+                                                   nil]];
+        
+        [[content layer] addSublayer:gradient];
+        
+        // Create a UILabel for the text
+        UILabel* textLabel = [[UILabel alloc] initWithFrame:CGRectMake(kGNTextAutocompleteCellLabelMargin,
+                                                                       kGNTextAutocompleteCellLabelMargin,
+                                                                       [self frame].size.width - 2 * kGNTextAutocompleteCellLabelMargin,
+                                                                       [self frame].size.height - 2 * kGNTextAutocompleteCellLabelMargin)];
+        [textLabel setText:text];
+        [textLabel setFont:[UIFont fontWithName:DEFAULT_FONT_FAMILY size:20]];
+        [textLabel setBackgroundColor:[UIColor clearColor]];
+        
+        [content addSubview:textLabel];
+        
+        [self addSubview:content];
+    }
+    return self;
+}
+
+-(void)gradientUpsideDown
+{
+    [CATransaction begin];
+    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+    [gradient setTransform:CATransform3DMakeRotation(M_PI, 0.0, 0.0, 1.0)];
+    [CATransaction commit];
+}
+
+-(void)gradientRightSideUp
+{
+    [CATransaction begin];
+    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+    [gradient setTransform:CATransform3DMakeRotation(0.0, 0.0, 0.0, 1.0)];
+    [CATransaction commit];
+}
+
+-(void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event
+{
+    [self gradientUpsideDown];
+}
+
+-(void)touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event
+{
+    [self gradientRightSideUp];
+}
+
+-(void)touchesCancelled:(NSSet*)touches withEvent:(UIEvent*)event
+{
+    [self gradientRightSideUp];
+}
+
+@end
